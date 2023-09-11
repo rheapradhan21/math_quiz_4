07a8344984cbdc9a1116f8adc230c5200e548012
rheapradhan21 <87064604+rheapradhan21@users.noreply.github.com>
2023-09-11 16:38:34 +0300

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -36,7 +36,7 @@ function check(){
 
     if(get_answer == actual_answer){
 
-        if(answer_turn = "player1"){
+        if(answer_turn == "player1"){
             player1_score = player1_score + 1;
             document.getElementById("player1_score").innerHTML = player1_score;
 
@@ -44,7 +44,7 @@ function check(){
         else{
             player2_score = player2_score + 1;
             document.getElementById("player2_score").innerHTML = player2_score;
-        }
+        }}
         if(question_turn == "player1"){
             question_turn = "player2";
             document.getElementById("player_question").innerHTML = "Question Turn -" + player2_name;
@@ -62,7 +62,7 @@ function check(){
             answer_turn = "player1";
             document.getElementById("player_answer").innerHTML = "Answer Turn -" + player1_name;
         }
-    }
+    
 
     
     document.getElementById("output").innerHTML = "";
